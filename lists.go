3b5602a0d4530b92def84a6cf74e2bd7a46a2811
axel paulander <axel.paulander@gmail.com>
2020-05-16 10:41:01 +0200

    Update lists.go

--- lists.go
+++ lists.go
@@ -1,5 +1,7 @@
 package synthax
 
+import "reflect"
+
 func Range(a, b, step int) []int {
 	out := []int{}
 	for i := a; i < b; i += step {
@@ -16,4 +18,43 @@ func RangeF(a, b, step float64) []float64 {
 	return out
 }
 
-//todo list comprehension
+func Map(set interface{}, fn func(x interface{}) interface{}) []interface{} {
+	setVal := reflect.ValueOf(set)
+	if setVal.Kind() != reflect.Slice {
+		return nil
+	}
+
+	out := []interface{}{}
+
+	for i := 0; i < setVal.Len(); i++ {
+		var x interface{} = setVal.Index(i)
+		if fn != nil {
+			x = fn(x)
+		}
+		out = append(out, x)
+	}
+
+	return out
+}
+
+func Filter(set interface{}, test func(x interface{}) bool) []interface{} {
+	setVal := reflect.ValueOf(set)
+	if setVal.Kind() != reflect.Slice {
+		return nil
+	}
+
+	out := []interface{}{}
+
+	for i := 0; i < setVal.Len(); i++ {
+		var x interface{} = setVal.Index(i)
+		if test == nil || test(x) {
+			out = append(out, x)
+		}
+	}
+
+	return out
+}
+
+func ListComp(fn func(x interface{}) interface{}, set interface{}, test func(x interface{}) bool) []interface{} {
+	return Map(Filter(set, test), fn)
+}
